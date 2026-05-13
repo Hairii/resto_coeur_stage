8e9e8daae85e4ed881a7202630414840bcc77475
Hairii <dugarcein.leila16@gmail.com>
2026-05-13 09:51:02 +0200

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,21 +2,23 @@ import pg from "pg";
 
 const { Pool } = pg;
 
-let db;
-try {
-  db = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 5432,
-    ssl: { rejectUnauthorized: false },
+const db = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 5432,
+  ssl: { rejectUnauthorized: false },
+});
+
+db.connect()
+  .then(client => {
+    console.log("Connexion à la base de données réussie");
+    client.release();
+  })
+  .catch(error => {
+    console.error("Erreur de connexion", error.message);
+    process.exit(1);
   });
 
-  const client = await db.connect();
-  console.log("Connexion à la base de données réussie");
-  client.release();
-} catch (error) {
-  console.error("Erreur de connexion", error.message);
-  process.exit(1);
-}
+export default db;
